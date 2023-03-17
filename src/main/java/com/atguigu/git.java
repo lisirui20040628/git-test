971b29c76b5bebbe7a3d4db0618cccbd245c40b1
lisirui <19868570180@163.com>
2023-03-17 13:10:29 +0800

    push commit

--- src/main/java/com/atguigu/git.java
+++ src/main/java/com/atguigu/git.java
@@ -4,5 +4,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("Hello git!");
         System.out.println("Hello git1!");
+        System.out.println("push git!");
     }
 }
